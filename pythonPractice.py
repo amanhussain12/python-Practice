3b308a626a117087c5a876f282d3af3bdc4e982d
Aman Hussain <amanhus977@gmail.com>
2026-07-02 04:36:14 +0500

    5th commit

--- pythonPractice.py
+++ pythonPractice.py
@@ -649,3 +649,59 @@
 
 # converter(price)
 
+#  home work problem solving
+
+# num = int(input("Enter a Num : "))
+
+# def explain(num):
+#     if(num % 2 == 0 ):
+#         print("EVEN NUMBER")
+#     else:
+#         print("ODD NUMBER")
+
+# explain(num)    
+
+
+    #  Recursion
+# when a functions calls itself repeatedly
+
+# def show(n):
+#     if n == 0:
+#         return
+#     print(n)
+#     show(n-1)
+
+# show(5) 
+
+# def fact(n):
+#     if n == 1 or n == 0:
+#         return 1
+#     return fact(n-1) * n
+
+# print(fact(7))
+
+# def cal_sum(n):
+#     if n == 0:
+#         return 0
+#     return cal_sum(n-1) + n
+
+# sum = cal_sum(50)
+# print(sum)
+
+# def cal_sum(n):
+#     if n == 1 or n == 0:
+#         return 0
+#     return cal_sum(n-1) - n
+
+# sum = cal_sum(5)
+# print(sum)
+
+
+def cal_list(list , idx=0):
+    if idx == len(list):
+        return
+    print(list[idx])
+    cal_list(list, idx+1)
+
+family = ["aman", "abad", "manahil", "mehwish", "rizwan", "pervaz"]
+cal_list(family)
